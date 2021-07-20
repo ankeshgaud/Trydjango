2858fe5197d686c066f01ac1d800edec4540b4e7
Ankesh Gaud <ankeshgaud@gmail.com>
2021-07-20 23:46:30 +0530

    7 commit

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at thedsada new polls index.")
+    return HttpResponse("Hello, world. You're hh at thedsada new polls index.")
 
 def test_index(request):
     return HttpResponse("You're at the new pollss ABDBDsba.")

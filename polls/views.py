e4094487b200cf2b7e5c3790a2d44adcb1ebe4f5
Ankesh Gaud <ankeshgaud@gmail.com>
2021-07-20 18:39:27 +0530

    third commit

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the new polls index.")
+
+def test_index(request):
+    return HttpResponse("Hello, world. You're at the new polls indessx.")
